d3188a204ff0486ef3a222f9bb43b8ae251d4b20
BC Holmes <github@bcholmes.org>
2023-01-14 17:50:41 -0500

    additional ship outlines; additional translations

--- WebTools/src/components/news.tsx
+++ WebTools/src/components/news.tsx
@@ -11,6 +11,10 @@ class News extends React.Component<INewsProperties, {}> {
     render() {
         return (
             <Modal size="lg" show={this.props.showModal} onClose={() => this.props.onClose()} header="What's New?">
+                <b>v1.230114</b>
+                <ul>
+                    <li>More translation work and minor bug fixes.</li>
+                </ul>
                 <b>v1.230113</b>
                 <ul>
                     <li>More testing with the star system diagram made me aware of a couple of minor bugs in the sector creation rules.
