51ae3c2fa13acce5c27eca135815b1fe600400ed
BC Holmes <github@bcholmes.org>
2023-05-21 10:41:42 -0400

    - record the ears; - add a Ro-style hair

--- WebTools/src/token/model/hairTypeEnum.ts
+++ WebTools/src/token/model/hairTypeEnum.ts
@@ -2,9 +2,10 @@
 export enum HairType {
     Bald, BowlCutHair, StylishHair, SidePart, ShortAfro, VeryShortAfro,
     Balding, StyledCurly, DeLeve, LongHair1, MediumLengthFemaleSidePart,
-    MediumLengthFemaleStraight,
+    MediumLengthFemaleStraight, ChinLengthCombBack
 }
 
 export const allHairTypes = [ HairType.Bald, HairType.BowlCutHair, HairType.StylishHair, HairType.SidePart, HairType.ShortAfro,
     HairType.VeryShortAfro, HairType.Balding,
-    HairType.StyledCurly, HairType.DeLeve, HairType.LongHair1, HairType.MediumLengthFemaleSidePart, HairType.MediumLengthFemaleStraight ];
+    HairType.StyledCurly, HairType.DeLeve, HairType.LongHair1, HairType.MediumLengthFemaleSidePart, HairType.MediumLengthFemaleStraight,
+    HairType.ChinLengthCombBack ];
