2f0d9c8c83b672573e3fd8dc519cbf67293e599c
BC Holmes <github@bcholmes.org>
2023-05-24 12:56:55 -0400

    another hair sample. Tweak one of the lipstick mouths to be a bit more narrow

--- WebTools/src/token/model/hairTypeEnum.ts
+++ WebTools/src/token/model/hairTypeEnum.ts
@@ -1,12 +1,12 @@
 
 export enum HairType {
     Bald, BowlCutHair, StylishHair, SidePart, ShortAfro, VeryShortAfro,
-    Balding, StyledCurly, DeLeve, LongHair1, MediumLengthFemaleSidePart,
+    Balding, Receding, StyledCurly, DeLeve, LongHair1, MediumLengthFemaleSidePart,
     MediumLengthFemaleStraight, ChinLengthCombBack, ShortTeasedOverEyeStyle, FeminineDreadStyle
 }
 
 export const allHairTypes = [ HairType.Bald, HairType.BowlCutHair, HairType.StylishHair, HairType.SidePart, HairType.ShortAfro,
-    HairType.VeryShortAfro, HairType.Balding,
+    HairType.VeryShortAfro, HairType.Balding, HairType.Receding,
     HairType.StyledCurly, HairType.DeLeve, HairType.LongHair1, HairType.MediumLengthFemaleSidePart, HairType.MediumLengthFemaleStraight,
     HairType.ChinLengthCombBack, HairType.ShortTeasedOverEyeStyle,
     HairType.FeminineDreadStyle ];
