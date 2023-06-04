e00b70abc5aa87d025b1d03b6242db512558f706
BC Holmes <github@bcholmes.org>
2023-06-04 12:32:11 -0400

    new hair styles; last remaining mouth.

--- WebTools/src/token/model/hairTypeEnum.ts
+++ WebTools/src/token/model/hairTypeEnum.ts
@@ -1,12 +1,13 @@
 
 export enum HairType {
-    Bald, BowlCutHair, StylishHair, SidePart, ShortAfro, VeryShortAfro,
-    Balding, Receding, StyledCurly, DeLeve, LongHair1, MediumLengthFemaleSidePart,
+    Bald, BowlCutHair, StylishHair, SidePart, ShortAfro, VeryShortAfro, PulledBackPonyTail,
+    Balding, Receding, StyledCurly, DeLeve, LongHair1, MediumLengthFemaleSidePart, MediumMaleCenterPart,
     MediumLengthFemaleStraight, ChinLengthCombBack, ShortTeasedOverEyeStyle, FeminineDreadStyle, ShoulderLengthMostlyStraight
 }
 
 export const allHairTypes = [ HairType.Bald, HairType.BowlCutHair, HairType.StylishHair, HairType.SidePart, HairType.ShortAfro,
-    HairType.VeryShortAfro, HairType.Balding, HairType.Receding,
-    HairType.StyledCurly, HairType.DeLeve, HairType.LongHair1, HairType.MediumLengthFemaleSidePart, HairType.MediumLengthFemaleStraight,
+    HairType.VeryShortAfro, HairType.PulledBackPonyTail, HairType.Balding, HairType.Receding,
+    HairType.StyledCurly, HairType.DeLeve, HairType.LongHair1, HairType.MediumLengthFemaleSidePart,
+    HairType.MediumMaleCenterPart, HairType.MediumLengthFemaleStraight,
     HairType.ChinLengthCombBack, HairType.ShortTeasedOverEyeStyle,
     HairType.FeminineDreadStyle, HairType.ShoulderLengthMostlyStraight ];
