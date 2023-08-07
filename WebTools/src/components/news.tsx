a9fcd0b393ea40da6e0ca019c52c9e8cbad7dd8c
BC Holmes <github@bcholmes.org>
2023-08-06 23:51:05 -0400

    new release

--- WebTools/src/components/news.tsx
+++ WebTools/src/components/news.tsx
@@ -12,6 +12,10 @@ class News extends React.Component<INewsProperties, {}> {
     render() {
         return (
             <Modal size="lg" show={this.props.showModal} onClose={() => this.props.onClose()} header="What's New?">
+                <b>v1.230806</b>
+                <ul>
+                    <li>Minor tweaks.</li>
+                </ul>
                 <b>v1.230805</b>
                 <ul>
                     <li>A few more lower decks options</li>
