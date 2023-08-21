3458b6603515d0921a413b9992f29ca1f217920e
BC Holmes <github@bcholmes.org>
2023-08-20 21:31:01 -0400

    news and release number

--- WebTools/src/components/news.tsx
+++ WebTools/src/components/news.tsx
@@ -12,6 +12,11 @@ class News extends React.Component<INewsProperties, {}> {
     render() {
         return (
             <Modal size="lg" show={this.props.showModal} onClose={() => this.props.onClose()} header="What's New?">
+                <b>v1.230820</b>
+                <ul>
+                    <li>More foundational work as prep for supporting Captain's Log features.</li>
+                    <li>New features for the token generator.</li>
+                </ul>
                 <b>v1.230812</b>
                 <ul>
                     <li>A few extra options on the Token Generator.</li>
