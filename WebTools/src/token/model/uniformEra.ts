e6989b88c00539ea538f0f01fb4fa8aea6dcd2e6
BC Holmes <github@bcholmes.org>
2024-01-11 23:38:14 -0500

    finish up the Suliban uniform; adjust the neck so that it's visible under the Suliban outfit, but that causes some other uniforms to need tweaks.

--- WebTools/src/token/model/uniformEra.ts
+++ WebTools/src/token/model/uniformEra.ts
@@ -40,11 +40,11 @@ export class UniformEraHelper {
         new UniformEraModel(UniformEra.Klingon, "Klingon Armour"),
         new UniformEraModel(UniformEra.DominionWar, "Dominion War"),
         new UniformEraModel(UniformEra.LowerDecks, "Lower Decks"),
-//        new UniformEraModel(UniformEra.JemHadar, "Jem'Hadar"),
+        new UniformEraModel(UniformEra.JemHadar, "Jem'Hadar"),
         new UniformEraModel(UniformEra.OriginalSeries, "Original Series"),
         new UniformEraModel(UniformEra.OriginalSeriesKlingon, "Klingon (Original Series)"),
         new UniformEraModel(UniformEra.Romulan, "Romulan"),
-//        new UniformEraModel(UniformEra.Suliban, "Suliban"),
+        new UniformEraModel(UniformEra.Suliban, "Suliban"),
         new UniformEraModel(UniformEra.VoyagerDS9, "Voyager/DS9"),
         new UniformEraModel(UniformEra.MonsterMaroon, "The Wrath of Khan and Later Movies"),
     ]
