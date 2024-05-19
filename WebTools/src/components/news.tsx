fd9aacfc3d6aa17320fa0cca979cb6f2cea360e0
BC Holmes <github@bcholmes.org>
2024-05-18 23:28:35 -0400

    new release

--- WebTools/src/components/news.tsx
+++ WebTools/src/components/news.tsx
@@ -12,6 +12,10 @@ class News extends React.Component<INewsProperties, {}> {
     render() {
         return (
             <Modal size="lg" show={this.props.showModal} onClose={() => this.props.onClose()} header="What's New?">
+                <b>v1.240518</b>
+                <ul>
+                    <li>Minor revisions.</li>
+                </ul>
                 <b>v1.240512</b>
                 <ul>
                     <li>The big new thing is that I've converted the one-page TNG Landscape PDF to better support
