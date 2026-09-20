6cce59cbe48071f746d48225d0176a0d3bef1ce5
BC Holmes <github@bcholmes.org>
2026-09-20 13:54:02 -0400

    Prodigy cadet variant uniform

--- WebTools/src/token/model/uniformVariantRestrictions.ts
+++ WebTools/src/token/model/uniformVariantRestrictions.ts
@@ -51,9 +51,7 @@ export class UniformVariantRestrictions {
         result.push(UniformVariantType.Variant2);
       }
     } else if (uniformEra === UniformEra.Prodigy) {
-      if (!isCadetRank(rank)) {
-        result.push(UniformVariantType.Variant1);
-      }
+      result.push(UniformVariantType.Variant1);
     } else if (uniformEra === UniformEra.StrangeNewWorlds) {
       if (DivisionColors.getDivision(uniformEra, divisionColor) === 'Medical') {
         result.push(UniformVariantType.Variant2);
